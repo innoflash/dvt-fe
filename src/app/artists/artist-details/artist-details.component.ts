73cebbe3c0f1c356b482351bd0f772bb2fbe27fe
innoflash <innocentmazando@gmail.com>
2022-02-22 19:36:39 +0200

    loading artist data

--- src/app/artists/artist-details/artist-details.component.ts
+++ src/app/artists/artist-details/artist-details.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
+import { AlbumModel } from '../../shared/models/album.model';
+import { ArtistModel } from '../../shared/models/artist.model';
+import { TrackModel } from '../../shared/models/track.model';
+import { AlbumsService } from '../services/albums.service';
 import { ArtistsService } from '../services/artists.service';
+import { TracksService } from '../services/tracks.service';
 
 @Component({
   selector: 'app-artist-details',
@@ -8,12 +14,17 @@ import { ArtistsService } from '../services/artists.service';
   styleUrls: ['./artist-details.component.scss']
 })
 export class ArtistDetailsComponent implements OnInit {
-  public isLoading = false;
   private artistId!: number;
+  public isLoading = false;
+  public artist?: ArtistModel;
+  public tracks: TrackModel[] = [];
+  public albums: AlbumModel[] = [];
 
   constructor(
     private route: ActivatedRoute,
-    private artistsService: ArtistsService
+    private artistsService: ArtistsService,
+    private tracksService: TracksService,
+    private albumsService: AlbumsService
   ) {
   }
 
@@ -26,7 +37,36 @@ export class ArtistDetailsComponent implements OnInit {
     });
   }
 
+  /**
+   * Loads the required artist details.
+   */
   private loadArtistDetails(): void {
-    this.artistsService.getArtistDetails(this.artistId).subscribe();
+    this.isLoading = true;
+
+    this.resetValues();
+
+    forkJoin([
+      this.artistsService.getArtistDetails(this.artistId),
+      this.tracksService.getData(this.artistId),
+      this.albumsService.getData(this.artistId)
+    ])
+      .subscribe({
+        next: ([artist, tracks, albums]) => {
+          this.artist = artist;
+          this.tracks = tracks;
+          this.albums = albums;
+        },
+        error: err => alert(err.message),
+        complete: () => this.isLoading = false
+      });
+  }
+
+  /**
+   * Resets the values in preparation for a re-render.
+   */
+  private resetValues(): void {
+    this.artist = undefined;
+    this.tracks = [];
+    this.albums = [];
   }
 }
