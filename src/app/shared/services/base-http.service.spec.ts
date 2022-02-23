61e246faf12285d6bc4de9c5edeeb20847a700c0
innoflash <innocentmazando@gmail.com>
2022-02-23 10:02:51 +0200

    tested base http service

--- src/app/shared/services/base-http.service.spec.ts
+++ src/app/shared/services/base-http.service.spec.ts
@@ -1,16 +1,28 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { BaseHttpService } from './base-http.service';
 
-describe('BaseHttpService', () => {
-  let service: BaseHttpService;
+fdescribe('BaseHttpService', () => {
+  let service: TestBaseHttpService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(BaseHttpService);
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers:[
+        TestBaseHttpService
+      ]
+    });
+    service = TestBed.inject(TestBaseHttpService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+  it('should be created', () => expect(service).toBeTruthy());
+
+  it('should append /api to the given route.', () => expect(service.url).toBe('api/url'));
 });
+
+class TestBaseHttpService extends BaseHttpService {
+  public url = this.resolveUrl('/url');
+};
