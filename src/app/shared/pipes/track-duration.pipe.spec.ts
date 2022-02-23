a12e20aff42634caa4c74e0738f10f5c3c79d0aa
innoflash <innocentmazando@gmail.com>
2022-02-23 09:39:20 +0200

    tested time duration transformer

--- src/app/shared/pipes/track-duration.pipe.spec.ts
+++ src/app/shared/pipes/track-duration.pipe.spec.ts
@@ -1,8 +1,16 @@
 import { TrackDurationPipe } from './track-duration.pipe';
 
-describe('TrackDurationPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TrackDurationPipe();
-    expect(pipe).toBeTruthy();
+fdescribe('TrackDurationPipe', () => {
+  let pipe: TrackDurationPipe;
+  beforeEach(() => {
+    pipe = new TrackDurationPipe();
   });
+
+  it('create an instance', () => expect(pipe).toBeTruthy());
+
+  it('should transform a time', () => expect(pipe.transform(90)).toBe('01:30'));
+
+  it('should pad minutes on time', () => expect(pipe.transform(40)).toBe('00:40'));
+
+  it('should pad seconds on time', () => expect(pipe.transform(3)).toBe('00:03'));
 });
